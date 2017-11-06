4634e64f81256ccf98ae8ede6e9169820611d790
omarart <om.sd2012@gmail.com>
2017-11-07 01:55:14 +0300

    API

---
+++ locationModel.js
@@ -0,0 +1,44 @@
+var initalLocations = [
+    {
+        name: 'King Saud University',
+        location: {lat: 24.742205,lng: 46.622934},
+        info: 'King Saud University',
+        id: 1,
+        category: 'KingSaudUniversity'
+    },
+    {
+        name: 'Fitness Time',
+        location: {lat: 24.743382, lng: 46.620692},
+        info: 'Fitness Time',
+        id: 2,
+        category: 'Fitness'
+    },
+    {
+        name: 'THE BOULEVARD - ALRYADH', 
+        location: {lat: 24.750641, lng: 46.613548},
+        info: 'THE BOULEVARD - ALRYADH',
+        id: 3,
+        category: 'ALRYADH'
+    },
+    {
+        name: 'Danub Market', 
+        location: {lat: 24.753019, lng: 46.610083},
+        info: 'Danub Market',
+        id: 4,
+        category: 'Market'
+    },
+    {
+        name: 'Rubeen Plaza',
+        location: {lat: 24.752980, lng: 46.624867},
+        info: 'Rubeen Plaza',
+        id: 5,
+        category: 'RubeenPlaza'
+    },
+    {
+        name: 'Badir Program', 
+        location: {lat: 24.747441, lng: 46.618933},
+        info: 'Badir Program',
+        id: 6,
+        category: 'BusinessIncubation'
+    }
+];
--- new.js
+++ new.js
@@ -1,45 +1,4 @@
 var map;
-
-// locations array
-var initalLocations = [
-    {
-        name: 'King Saud University',
-        location: {lat: 24.742205,lng: 46.622934},
-        info: 'King Saud University',
-        id: 1
-    },
-    {
-        name: 'Fitness Time',
-        location: {lat: 24.743382, lng: 46.620692},
-        info: 'Fitness Time',
-        id: 2
-    },
-    {
-        name: 'THE BOULEVARD - ALRYADH', 
-        location: {lat: 24.750641, lng: 46.613548},
-        info: 'THE BOULEVARD - ALRYADH',
-        id: 3
-    },
-    {
-        name: 'Danub Market', 
-        location: {lat: 24.753019, lng: 46.610083},
-        info: 'Danub Market',
-        id: 4
-    },
-    {
-        name: 'Rubeen Plaza',
-        location: {lat: 24.752980, lng: 46.624867},
-        info: 'Rubeen Plaza',
-        id: 5
-    },
-    {
-        name: 'Badir Program', 
-        location: {lat: 24.747441, lng: 46.618933},
-        info: 'Badir Program',
-        id: 6
-    }
-];
-
 Location = function (data) {
     var that = this;
 
@@ -47,27 +6,13 @@ Location = function (data) {
     this.position = data.location;
     this.info = ko.observable(data.info);
     this.id = ko.observable(data.id);
-    this.imgList = ko.observableArray([]);
+    this.category = ko.observable(data.category);
+    this.Wikipedia = '';
 
     this.infoWindow = new google.maps.InfoWindow({
-        maxWidth: 200,
-        content: that.infoWin
+        maxWidth: 350,
+        content: that.information
     });
-
-    // for(var i =0 ; i < initalLocations.length ; i++ ) {
-    //     var name = initalLocations[i].name;
-    //     var position = initalLocations[i].location;
-    //     var info = initalLocations[i].info;
-
-    //     this.marker = new google.maps.Marker({
-    //         map: map,
-    //         position: position,
-    //         title: name,
-    //         animation: google.maps.Animation.DROP,
-    //     });
-    //     this.marker.addListener('click', that.clicker);
-        
-    // }
 
     this.marker = new google.maps.Marker({
         map: that.map,
@@ -76,57 +21,77 @@ Location = function (data) {
         id: that.id(),
         animation: google.maps.Animation.DROP,
     });
+
     
-    this.show = ko.observable(true);
-    if (this.show() === true )
-        that.marker.setMap(map);
-    else
-        that.marker.setMap(null);
-
-    map.addListener('zoom_changed', function() {
-    infowindow.setContent('Zoom: ' + map.getZoom());
-    });
-
-    this.clicker = function() {
-        that.infoWin = 
-        '<div class="info-content">' +'<h3>' + that.name() + '</h3>' +'<p>' + that.info() + '</p>'+'</div>';
-        that.infoWindow.setContent(that.infoWin);
+    this.wikipedia = function(marker){
+        var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+that.category() + '&format=json&callback=wikiCallback';
+        $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'jsonp'
+        })
+        .done(function(reply) {
+            var name = reply[0];
+            that.Wikipedia = reply[2];
+            var slice = that.Wikipedia.slice(0,2);
+            slice.slice(0,2);
+            console.log(that.Wikipedia);
+            console.log(slice);
+            var blog = 'http://en.wikipedia.org/wiki/' + that.category();
+            that.information = '<div class="info-content"><h3>' + name + '</h3>' + '<p>' + slice +'</p><p><b>More Visit</b>: <a href="' + blog + '">' + name + '</a></p></div>';
+            that.infoWindow.setContent(that.information);
+            console.log('sec');
+        })
+        .fail(function(){
+            that.information = 'sorry, the api dont work';
+            that.infowindow.setContent(that.information);
+            console.log('the api dont work');
+            $('#map').html('There is an error with wikipedia API ');
+        })
+        .always(function(){
+            console.log('that is better API');
+        });
+      };
+    
+    this.clicker = function(mark) {
+        that.wikipedia(mark);
         that.infoWindow.open(map, this);
+        console.log('Good');
         that.animationBounce();
-        // that.closeWin(that.marker.id);
     };
     this.marker.addListener('click', that.clicker);
-
-    // this.closeWin = function(id) {
-    //     if (that.id !== id){
-    //         that.infoWindow.closeclick();
-    //     }
-    // };
 
     this.animationBounce = function() {
         that.marker.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout(function() {
             that.marker.setAnimation(null);
         }, 2000);
-    };    
+    };
 
     this.clicked = function(marker) {
         google.maps.event.trigger(that.marker, 'click');
     };
 
+    this.showMarks = ko.observable(true);        
+    this.show = ko.computed(function(){
+        if(this.showMarks() !== true)
+            that.marker.setMap(null);
+        else
+            that.marker.setMap(map);
+    }, this);
+
 };
 
 function ViewModel(){
     var that = this;
-
     this.LocationList = ko.observableArray([]);
     that.imgList = ko.observableArray([]);
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 24.746726, lng: 46.618246},
-        zoom: 15,
-        mapTypeControl: false,
-        styles: styles
-    });
+
+    this.resetMap = function() {
+            map.setCenter({lat: 24.746726, lng: 46.618246});
+            map.setZoom(13);
+            console.log('s');            
+    };
 
     var styles = [
         {
@@ -195,27 +160,32 @@ function ViewModel(){
         }
     ];
 
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 24.746726, lng: 46.618246},
+        zoom: 14,
+        mapTypeControl: false,
+        styles: styles
+    });
+
     initalLocations.forEach(function(locationItem){
         that.LocationList.push( new Location(locationItem) );        
-    });    
+    });
 
-    that.filterSearch = ko.observable();     
+    that.filterSearch = ko.observable('');
     this.search = ko.computed(function() {
-        var newFilter = that.filterSearch();
-        if ( newFilter !== false){
-            newFilter = that.filterSearch();
+        var searchs= that.filterSearch();
+        if ( searchs !== false){
             return ko.utils.arrayFilter(that.LocationList(), function(place) {
-                var title = place.name();
-                var outs = title.search(newFilter) >= 0;
+                var outs = place.name().toLowerCase().indexOf(that.filterSearch().toLowerCase()) >= 0;
+                place.showMarks(outs);
                 return outs;
             });
         } else {
             that.LocationList().forEach(function(place){
+                place.showMarks(true);
             });
-            return that.LocationList();
         }
     });
-
 }
 
 function googleMapsError(){
